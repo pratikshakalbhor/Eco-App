b8c58b4562d8fba9d14bb531ea099cf6790763a5
pratikshakalbhor <kalbhorpratiksha333@gmail.com>
2026-06-10 10:23:51 +0530

    Commit for Backend/api-server/services/blockchain_service.go

--- Backend/api-server/services/blockchain_service.go
+++ Backend/api-server/services/blockchain_service.go
@@ -8,7 +8,6 @@ import (
 	"os"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
